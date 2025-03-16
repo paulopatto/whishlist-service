29c3230d74f0dbfaea5614377fd592408ceb69e8
Paulo Patto <paulopatto@paulopatto.com>
2025-03-16 18:08:45 -0300

    feat(healthcheck): Add check to postgres

--- src/healthcheck/routes.py
+++ src/healthcheck/routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from .services import check_status
+from src.healthcheck.interfaces import IHealthCheckResult
+
+from .services import check_status, database_is_health
 
 router = APIRouter()
 
@@ -8,4 +10,16 @@ router = APIRouter()
 @router.get("/api/healthcheck", description="Check the health of the API")
 async def healthcheck():
     status = check_status()
-    return {"status": "ok", "details": status}
+    return { "status": "ok", "details": status }
+
+
+@router.get(
+    "/api/healthcheck/database",
+    description="Check the health of database connection"
+)
+async def check_database_connection():
+    status: IHealthCheckResult = database_is_health()
+    if not status.health:
+        raise HTTPException(status_code=503, detail=status.message)
+    return { "status": "ok", "details": status }
+
--- src/healthcheck/services.py
+++ src/healthcheck/services.py
@@ -1,2 +1,25 @@
+import os
+
+import psycopg2
+
+from src.healthcheck.interfaces import HealthCheckResultDTO, IHealthCheckResult
+
+
 def check_status():
     return "Everything is ok"
+
+
+def database_is_health() -> IHealthCheckResult:
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    if not DATABASE_URL:
+        return HealthCheckResultDTO(health=False, message="DATABASE_URL not set")
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        conn.close()
+        return HealthCheckResultDTO(health=True, message="Database is healthy")
+    except Exception as e:
+        error_message = str(e)
+        print(f"Database connection error: {error_message}")
+        return HealthCheckResultDTO(health=False, message=error_message)
+
